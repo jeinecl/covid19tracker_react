60223de6a5e3fc28511dd1d44d93ca02d7c740e4
jeinecl <jeineclaudia@gmail.com>
2020-12-08 12:37:27 +0800

    using bootstrap

--- src/App.js
+++ src/App.js
@@ -3,23 +3,25 @@ import {Switch, Route, Link} from 'react-router-dom'
 import global from './global'
 import indonesia from './indonesia'
 import provinsi from './provinsi'
+import {Navbar, Nav} from 'react-bootstrap'
+//import Card from './component/Card'
+
 
 const App = () => {
   return (
+   // <Card />
     <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/' >Global</Link>
-          </li>
-          <li>
-            <Link to='/indonesia' >Indonesia</Link>
-          </li>
-          <li>
-            <Link to='/provinsi' >Provinsi</Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar.Brand href=".">Covid 19 Tracker</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href=".">Global</Nav.Link>
+      <Nav.Link href="/indonesia">Indonesia</Nav.Link>
+      <Nav.Link href="/provinsi">Provincy</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+</Navbar> 
       <Switch>
         <Route exact path = '/' component={global}/>
         <Route path= '/indonesia' component={indonesia}/>
